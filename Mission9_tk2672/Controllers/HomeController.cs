61fcd461184944be602680f1db960fc805ebe4ee
Tyler Kendall <tylerchristiankendall@gmail.com>
2023-02-27 21:34:00 -0700

    database items display on page

---
+++ Mission9_tk2672/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mission9_tk2672.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_tk2672.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+
+        public HomeController (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = context.Books.ToList();
+
+            return View(blah);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Mission9_tk2672/Models/BookstoreContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mission9_tk2672.Models
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext()
+        {
+        }
+
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
