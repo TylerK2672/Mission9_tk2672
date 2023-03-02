0ea2461c5190218a00e7940690155c53ea11d51b
Tyler Kendall <tylerchristiankendall@gmail.com>
2023-03-01 21:36:54 -0700

    finished bootstrap section

--- Mission9_tk2672/Controllers/Infrastructure/PaginationTagHelper.cs
+++ Mission9_tk2672/Controllers/Infrastructure/PaginationTagHelper.cs
@@ -26,6 +26,10 @@ namespace Mission9_tk2672.Controllers.Infrastructure
         //different than the view context
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
 
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
@@ -38,6 +42,11 @@ namespace Mission9_tk2672.Controllers.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
